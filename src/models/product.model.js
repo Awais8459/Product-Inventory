cd9ada6817b2714a03c99d305f165a930408f3f9
Awais8459 <awaisfarooq010@gmail.com>
2023-07-11 14:54:06 +0500

    products model

---
+++ src/models/product.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+const { roles } = require('../config/roles');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    sku: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
+/**
+ * @typedef Product
+ */
+const Product = mongoose.model('Product', userSchema);
+
+module.exports = Product;

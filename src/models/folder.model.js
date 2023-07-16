0d6fdcfb39963697ad11dd807643bf1f3792c04f
Awais8459 <awaisfarooq010@gmail.com>
2023-07-16 22:42:44 +0500

    commit

--- src/models/folder.model.js
+++ src/models/folder.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Define the schema for the Folder model
-const folderSchema = new mongoose.Schema({
+const folderSchema = new Schema({
   name: { type: String },
   parent: { type: Schema.Types.ObjectId, ref: 'Folder', default: null },
   parent_list: [{ type: Schema.Types.ObjectId, ref: 'Folder' }],

33ff2c8afd067dfe4c57d6e374703baee98f32c3
Awais8459 <awaisfarooq010@gmail.com>
2023-07-17 16:10:34 +0500

    create, upload and delete feature for folders

--- src/models/folder.model.js
+++ src/models/folder.model.js
@@ -6,5 +6,6 @@ const folderSchema = new Schema({
   name: { type: String },
   parent: { type: Schema.Types.ObjectId, ref: 'Folder', default: null },
   parent_list: [{ type: Schema.Types.ObjectId, ref: 'Folder' }],
+  images: [{ type: Schema.Types.ObjectId, ref: 'Image' }],
 });
 module.exports = mongoose.model('Folder', folderSchema);
--- src/services/folder.service.js
+++ src/services/folder.service.js
@@ -14,60 +14,6 @@ exports.uploadFolder = async (name, parentId) => {
   return await folder.save();
 };
 
-// exports.createFolder = async (name, parent_list) => {
-//   const newFolder = await Folder.create({
-//     name: name,
-//     parent: parentId,
-//     parent_list: [],
-//   });
-//   return newFolder;
-// };
-
-// exports.createFolder = async (name, parentId) => {
-//   const newFolder = new Folder({
-//     name: name,
-//     parent: parentId,
-//     parent_list: [],
-//   });
-//   return await newFolder.save();
-// };
-
-
-// exports.deleteFolder = async (folderId) => {
-//   // Find the folder to delete
-//   const folderToDelete = await Folder.findById(folderId);
-
-//   if (!folderToDelete) {
-//     throw new Error('Folder not found');
-//   }
-
-  // Delete the folder and its children recursively
-//   exports.deleteFolder = async (folderId) => {
-//     const folderToDelete = await Folder.findById(folderId);
-  
-//     if (!folderToDelete) {
-//       throw new Error('Folder not found');
-//     }
-  
-//     await deleteFolderAndChildren(folderToDelete);
-  
-//     // Delete the current folder
-//     await Folder.findByIdAndDelete(folderId);
-//   };
-  
-//   async function deleteFolderAndChildren(folder) {
-//     // Delete children folders recursively
-//     for (const childId of folder.parent_list) {
-//       const childFolder = await Folder.findById(childId);
-//       if (childFolder) {
-//         await deleteFolderAndChildren(childFolder);
-//         await Folder.findByIdAndDelete(childFolder._id);
-//       }
-//     }
-//   } 
-// }
-
-
 exports.deleteFolder = async (folderId) => {
   // Find the folder to delete
   const folderToDelete = await Folder.findById(folderId);
@@ -79,6 +25,13 @@ exports.deleteFolder = async (folderId) => {
   // Get all child folders recursively
   const childFolders = await Folder.find({ parent_list: folderId });
 
+  const images = await Image.find({ parent: folderId });
+
+    // Delete images within the folder
+    for (const image of images) {
+      await Image.findByIdAndDelete(image._id);
+    }
+
   // Delete child folders recursively
   for (const childFolder of childFolders) {
     await deleteFolderAndChildren(childFolder._id);
@@ -86,6 +39,7 @@ exports.deleteFolder = async (folderId) => {
 
   // Delete the current folder
   await Folder.findByIdAndDelete(folderId);
+
 };
 
 async function deleteFolderAndChildren(folderId) {
@@ -93,10 +47,10 @@ async function deleteFolderAndChildren(folderId) {
   const childFolders = await Folder.find({ parent_list: folderId });
 
   // Delete child folders recursively
-  for (const childFolder of childFolders) {
+  for (const childFolder of childFolders) { 
     await deleteFolderAndChildren(childFolder._id);
   }
-
   // Delete the current folder
   await Folder.findByIdAndDelete(folderId);
 }
+

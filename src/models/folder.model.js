2eb9ec2095787e88bb53678774422b55e7d2a3b0
Awais8459 <awaisfarooq010@gmail.com>
2023-07-25 17:19:50 +0500

    Model

--- src/models/folder.model.js
+++ src/models/folder.model.js
@@ -3,9 +3,9 @@ const { Schema } = mongoose;
 
 // Define the schema for the Folder model
 const folderSchema = new Schema({
-  name: { type: String },
+  name: { type: String, default: "new folder" },
   parent: { type: Schema.Types.ObjectId, ref: 'Folder', default: null },
   parent_list: [{ type: Schema.Types.ObjectId, ref: 'Folder' }],
-  images: [{ type: Schema.Types.ObjectId, ref: 'Image' }],
+  // images: [{ type: Schema.Types.ObjectId, ref: 'Image' }],
 });
 module.exports = mongoose.model('Folder', folderSchema);

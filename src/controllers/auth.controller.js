64e5bf680905ca789789c7fb6805fa09a6c9f657
Awais8459 <awaisfarooq010@gmail.com>
2023-09-06 13:07:42 +0500

    Auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,8 @@ const login = catchAsync(async (req, res) => {
 
 const logout = catchAsync(async (req, res) => {
   await authService.logout(req.body.refreshToken);
-  res.status(httpStatus.NO_CONTENT).send();
+  // res.status(httpStatus.NO_CONTENT).send();
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 const refreshTokens = catchAsync(async (req, res) => {
